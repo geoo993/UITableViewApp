fa8091e4102610c3fa7d75bb4af02cb2c96fb4ae
GEORGE QUENTIN <ggeoo93@gmail.com>
2016-10-15 16:40:29 +0100

    added new images

---
+++ UITableViewApp/UITableViewApp/CustomProductTableViewController.swift
@@ -0,0 +1,115 @@
+//
+//  NewTableViewController.swift
+//  UITableViewApp
+//
+//  Created by GEORGE QUENTIN on 15/10/2016.
+//  Copyright © 2016 GEORGE QUENTIN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class NewTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.randomColor()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        
+        guard let cell = tableView.dequeueReusableCellWithIdentifier("NewCell", forIndexPath: indexPath) as? MediaTableViewCell
+         //let cell = tableView.dequeueReusableCellWithIdentifier("NewCell") as? aCell 
+        else 
+        { print("No Cell found"); return UITableViewCell()}
+        
+        //// Configure the cell...
+
+        ////titleText.text = product.title
+        
+        ////descriptionText.text = product.description
+        
+        ////let img = product.image.imageWithSize(CGSize(width: 50,height: 100), extraMargin: 0)
+        ////imgView.image = img
+        
+        cell.imgView.image = UIImage(named: "Casino.jpg") 
+        return cell
+    }
+    
+    //func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat 
+    //{
+        //return 200.0;//Choose your custom row height
+    //}
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
